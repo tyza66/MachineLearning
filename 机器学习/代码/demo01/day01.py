b5fa68e17d4094f8a2f36937bb853a5e16706775
洮羱芝闇 <shun_@outlook.com>
2022-05-08 16:34:28 +0800

    update

---
+++ 机器学习/代码/demo01/day01.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+
+def datasets_demo():
+    #数据集的使用
+    iris = load_iris()
+    print(iris)
+    return None
+
+
+if __name__ == '__main__':
+    datasets_demo()
