0beb74423e32b50ee48db1f43d0e8453bdc48b3c
洮羱芝闇 <shun_@outlook.com>
2022-05-09 15:40:29 +0800

    update

--- 机器学习/代码/demo01/day01.py
+++ 机器学习/代码/demo01/day01.py
@@ -40,10 +40,16 @@ def cut_word(text):
 
 def count_chinese_demo():
     #中文文本特征抽取 自动分词
-    data = []
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。",
+            "我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。"]
     data_new = []
     for sent in data:
         data_new.append(cut_word(sent))
+    transfer = CountVectorizer()
+    data_final = transfer.fit_transform(data_new)
+    print(data_final.toarray())
+    print("特征名：", transfer.get_feature_names_out())
     return None
 
 if __name__ == '__main__':
